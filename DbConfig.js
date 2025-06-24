7e7a23c1b62499e91f234ef62c671868df39191f
Karishma Shinde <108576677+Karishma7720@users.noreply.github.com>
2025-06-24 13:21:29 +0530

    Add files via upload

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-2-instance-1.c1icoyy4swxt.eu-west-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678910',
+    DB_DATABASE : 'webappdb'
+});
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 3-tierArchitecture
-A resilient AWS three-tier architecture
+# Web Tier
+
